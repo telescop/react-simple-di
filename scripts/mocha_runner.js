aba4eaa9b22c3bf3005202e5e015a371829ac3d7
Guillaume Delannoy <guillaume@telescop.com>
2023-03-27 16:12:35 +0000

    React 17.

--- scripts/mocha_runner.js
+++ scripts/mocha_runner.js
@@ -1,5 +1,5 @@
-require('babel-core/register');
-require('babel-polyfill');
+require('@babel/register');
+require('@babel/polyfill');
 
 process.on('unhandledRejection', function (error) {
   console.error('Unhandled Promise Rejection:');
